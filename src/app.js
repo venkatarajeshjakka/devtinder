13eb6e5948cab77586444a85e201ab21ab680487
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2025-09-23 19:59:46 +0530

    added content type

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3040;
 
 app.get("/", (req, res) => {
+  res.contentType("application/json");
   res.send("Hello to my server!");
+});
+
+app.get("/about", (req, res) => {
+  res.contentType("application/json");
+  res.send("This is the about page.");
 });
 
 app.listen(port, () => {
