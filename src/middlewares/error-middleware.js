4fb02970091a5c7e34f6dde1f83ab716b1c587a5
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2025-09-25 21:17:23 +0530

    updated the error middleware

--- src/middlewares/error-middleware.js
+++ src/middlewares/error-middleware.js
@@ -1,7 +1,11 @@
 const errorMiddleware = (err, req, res, next) => {
   if (err) {
     console.error(`Error in the stack:${err.stack}`);
-    res.status(500).send(`Something broke! you requested URL is ${req.url}`);
+    return res.status(500).json({
+      success: false,
+      errorType: "InternalServerError",
+      message: "Something went wrong",
+    });
   }
 };
 
